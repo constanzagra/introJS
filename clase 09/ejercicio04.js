4065eb585b08bdf8d5200d98c3a95ee13d4e5803
constanzagra <riverosaconstanza@gmail.com>
2024-11-15 16:55:38 -0300

    agregue el toLowerCase

--- clase 09/ejercicio04.js
+++ clase 09/ejercicio04.js
@@ -2,8 +2,8 @@
 //Declara una variable nombre y pide al usuario que ingrese su nombre. 
 //Verifica si el nombre ingresado es igual a tu nombre.
 const prompt = require('prompt-sync')();
-let nombre = prompt("Ingrese su nombre");
-let miNombre = "Constanza";
+let nombre = prompt("Ingrese su nombre").toLowerCase();
+let miNombre = "constanza";
 if(nombre == miNombre){
     console.log("Ay boluda yo me llamo igual")
 }else{
